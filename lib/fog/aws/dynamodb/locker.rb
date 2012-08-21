c3dba766b21f478d1c403e9e7a8e2f65f4ddcf7a
Michael Gorsuch <michael.gorsuch@gmail.com>
2012-08-21 10:45:57 -0500

    skel

---
+++ lib/fog/aws/dynamodb/locker.rb
@@ -0,0 +1,10 @@
+require "fog/aws/dynamodb/locker/version"
+
+module Fog
+  module AWS
+    class Dynamodb
+      module Locker
+      end
+    end
+  end
+end
---
+++ lib/fog/aws/dynamodb/locker/version.rb
@@ -0,0 +1,9 @@
+module Fog
+  module AWS
+    class DynamoDB
+      module Locker
+        VERSION = "0.0.1"
+      end
+    end
+  end
+end
